5723eb63c30829a92e2df17cf6c9253834245752
Akira Morales <akira.k.morales@gmail.com>
2018-10-28 21:14:50 -0400

    Add files via upload

---
+++ src/Dice.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author Akira Morales
+ * Dice
+ * Roll, numRoll, Reset
+ */
+public class Dice
+{
+
+	private int rollCount;
+	
+	public Dice()
+	{
+		rollCount = 0;
+	}
+	
+	public int roll()
+	{		
+		rollCount++;
+		
+		return (int)(Math.random()*6+1);
+		
+	}
+	
+	public int numRoll()
+	{
+		return rollCount;
+	}
+	
+	public void reset()
+	{
+		rollCount = 0;
+	}
+}
---
+++ src/DiceArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * 
+ * @author Akira Morales
+ * Dice Array Homework
+ * Period 6
+ */
+public class DiceArray
+{
+	public static void main(String[] args)
+	{
+		Dice die1 = new Dice();
+		Dice die2 = new Dice();
+		int[] counter = new int[13];
+		
+		for(int i =0; i<counter.length; i++)
+		{
+			counter[i] = 0;
+		}
+		
+		for(int i=0; i<999; i++)
+		{
+			int val1 = die1.roll();
+			int val2 = die2.roll();
+			counter[val1] ++;
+			counter[val2] ++;
+		}
+		
+		System.out.print(Arrays.toString(counter));
+	}
+}
